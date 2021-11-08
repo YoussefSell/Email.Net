6ee0a59ec049ecbd2e870c4f2eb053ce191ab48a
Youssef sellami <youssef-sellami@outlook.com>
2021-11-08 21:17:16 +0100

    added an exception to be thrown when a option value is not specified

---
+++ Email.NET/Exceptions/RequiredOptionValueNotSpecifiedException.cs
@@ -0,0 +1,33 @@
+﻿namespace Email.NET.Exceptions
+{
+    using System;
+
+    /// <summary>
+    /// exception thrown when a required exception value is not specified
+    /// </summary>
+    [Serializable]
+    public class RequiredOptionValueNotSpecifiedException : Exception
+    {
+        /// <inheritdoc/>
+        public RequiredOptionValueNotSpecifiedException(Type optionsType, string optionsName, string message) : base(message)
+        {
+            OptionsType = optionsType;
+            OptionsName = optionsName;
+        }
+
+        /// <inheritdoc/>
+        protected RequiredOptionValueNotSpecifiedException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+
+        /// <summary>
+        /// the option object type
+        /// </summary>
+        public Type OptionsType { get; }
+
+        /// <summary>
+        /// option parameter name
+        /// </summary>
+        public string OptionsName { get; }
+    }
+}
