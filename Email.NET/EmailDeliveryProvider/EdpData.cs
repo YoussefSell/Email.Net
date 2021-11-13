10988b03842e152b3b07001c5356156ae575feff
youssef sellami <youssef-sellami@outlook.com>
2021-11-13 23:00:00 +0100

    added a function to check if the edp data is empty, and a function to get the value instance

--- Email.NET/EmailDeliveryProvider/EdpData.cs
+++ Email.NET/EmailDeliveryProvider/EdpData.cs
@@ -1,13 +1,29 @@
 ﻿namespace Email.NET.EDP
 {
-    using Utilities;
     using System;
     using System.Collections.Generic;
+    using Utilities;
 
     /// <summary>
     /// the email delivery provider data
     /// </summary>
-    public struct EdpData : IEquatable<EdpData>
+    public partial struct EdpData : IEquatable<EdpData>
+    {
+        /// <summary>
+        /// Gets the key of the data.
+        /// </summary>
+        public string Key { get; }
+
+        /// <summary>
+        /// Gets the value of the data.
+        /// </summary>
+        public object Value { get; }
+    }
+
+    /// <summary>
+    /// partial part for <see cref="EdpData"/>
+    /// </summary>
+    public partial struct EdpData : IEquatable<EdpData>
     {
         /// <summary>
         /// the email delivery provider data
@@ -26,16 +42,6 @@
         }
 
         /// <summary>
-        /// Gets the key of the data.
-        /// </summary>
-        public string Key { get; }
-
-        /// <summary>
-        /// Gets the value of the data.
-        /// </summary>
-        public object Value { get; }
-
-        /// <summary>
         /// create a new instance of <see cref="EdpData"/>
         /// </summary>
         /// <param name="key">the data key</param>
@@ -43,6 +49,19 @@
         /// <returns>instance of <see cref="EdpData"/></returns>
         public static EdpData New(string key, object value)
             => new EdpData(key, value);
+
+        /// <summary>
+        /// get the value
+        /// </summary>
+        /// <typeparam name="TValue">the type of the value</typeparam>
+        /// <returns>the value instance</returns>
+        public TValue GetValue<TValue>() => (TValue)Value;
+
+        /// <summary>
+        /// Indicates whether the specified edp is empty.
+        /// </summary>
+        /// <returns>true if the value is empty; otherwise, false.</returns>
+        public bool IsEmpty() => !Key.IsValid() && Value == default;
 
         /// <inheritdoc/>
         public override string ToString() => $"key: {Key} | value: {Value}";
@@ -80,5 +99,16 @@
 
         /// <inheritdoc/>
         public static bool operator !=(EdpData left, EdpData right) => !(left == right);
+
+        /// <summary>
+        /// holds some predefined <see cref="EdpData"/> keys
+        /// </summary>
+        public static class Keys
+        {
+            /// <summary>
+            /// edp data for the smtp options
+            /// </summary>
+            public const string SmtpOptions = "smtp_options";
+        }
     }
 }
