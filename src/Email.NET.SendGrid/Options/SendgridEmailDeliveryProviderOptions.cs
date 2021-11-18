e4122c11af310dbbfa1eef202b9faedab197510f
Youssef sellami <youssef-sellami@outlook.com>
2021-11-18 18:41:07 +0100

    added the sending logic for sendgrid

--- src/Email.NET.SendGrid/Options/SendgridEmailDeliveryProviderOptions.cs
+++ src/Email.NET.SendGrid/Options/SendgridEmailDeliveryProviderOptions.cs
@@ -1,16 +1,27 @@
 ﻿namespace Email.NET.EDP.SendGrid
 {
+    using Email.NET.EDP.Smtp;
+    using Email.NET.Exceptions;
+    using System.Collections.Generic;
+
     /// <summary>
     /// the options for configuring the Smtp email delivery provider
     /// </summary>
     public class SendgridEmailDeliveryProviderOptions
     {
         /// <summary>
+        /// Get or set your Twilio SendGrid Api key.
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
         /// validate if the options are all set correctly
         /// </summary>
         internal void Validate()
         {
-            
+            if (string.IsNullOrWhiteSpace(ApiKey))
+                throw new RequiredOptionValueNotSpecifiedException<SmtpOptions>(
+                    $"{nameof(ApiKey)}", "the given RequiredOptionValueNotSpecifiedException.ApiKey value is null or empty.");
         }
     }
 }
--- src/Email.NET.SendGrid/SendgridEmailDeliveryProvider.cs
+++ src/Email.NET.SendGrid/SendgridEmailDeliveryProvider.cs
@@ -1,27 +1,56 @@
 ﻿namespace Email.NET.EDP.SendGrid
 {
-    using System;
     using global::SendGrid;
-    using System.Threading.Tasks;
-using global::SendGrid.Helpers.Mail;
-using System.Linq;
+    using global::SendGrid.Helpers.Mail;
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
 
-/// <summary>
-/// the SendGrid client email delivery provider
-/// </summary>
+    /// <summary>
+    /// the SendGrid client email delivery provider
+    /// </summary>
     public partial class SendgridEmailDeliveryProvider : ISendgridEmailDeliveryProvider
     {
         /// <inheritdoc/>
         public EmailSendingResult Send(Message message, params EdpData[] data)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var client = CreateClient(data);
+                var mailMessage = CreateMessage(message, data);
+
+                var response = client.SendEmailAsync(mailMessage)
+                    .ConfigureAwait(false)
+                    .GetAwaiter().GetResult();
+
+                // build the result object and return
+                return BuildResultObject(response);
+            }
+            catch (Exception ex)
+            {
+                return EmailSendingResult.Failure(Name).AddError(ex);
+            }
         }
 
         /// <inheritdoc/>
-        public Task<EmailSendingResult> SendAsync(Message message, params EdpData[] data)
+        public async Task<EmailSendingResult> SendAsync(Message message, params EdpData[] data)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                var client = CreateClient(data);
+                var mailMessage = CreateMessage(message, data);
+
+                var response = await client.SendEmailAsync(mailMessage)
+                    .ConfigureAwait(false);
+
+                // build the result object and return
+                return BuildResultObject(response);
+            }
+            catch (Exception ex)
+            {
+                return EmailSendingResult.Failure(Name).AddError(ex);
+            }
         }
     }
 
@@ -50,6 +79,32 @@ using System.Linq;
             _options = options;
         }
 
+        private SendGridClient CreateClient(EdpData[] data)
+        {
+            var apiKey = _options.ApiKey;
+
+            // get the apiKey & serverId from the data list if any.
+            var apikeyEdpData = data.GetData(EdpData.Keys.ApiKey);
+
+            if (!apikeyEdpData.IsEmpty())
+                apiKey = apikeyEdpData.GetValue<string>();
+
+            return new SendGridClient(apiKey);
+        }
+
+        private static EmailSendingResult BuildResultObject(Response result)
+        {
+            // check if we have success operations
+            if (result.IsSuccessStatusCode)
+            {
+                // return the result
+                return EmailSendingResult.Success(Name);
+            }
+
+            // create the failure result & return the result
+            return EmailSendingResult.Failure(Name)
+                .AddMetaData("status_code", result.StatusCode.ToString());
+        }
 
         /// <summary>
         /// create an instance of <see cref="BasicMessage"/> from the given <see cref="Message"/>.
@@ -57,7 +112,7 @@ using System.Linq;
         /// <param name="message">the message instance</param>
         /// <param name="data">the edp data instance</param>
         /// <returns>instance of <see cref="BasicMessage"/></returns>
-        public SendGridMessage CreateBasicMessage(Message message, EdpData[] data)
+        public SendGridMessage CreateMessage(Message message, EdpData[] data)
         {
             var mailMessage = new SendGridMessage
             {
