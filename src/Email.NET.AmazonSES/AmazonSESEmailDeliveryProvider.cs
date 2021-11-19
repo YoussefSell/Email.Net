902f4f932562925fe7d226ec9bbc480c6ff423d2
Youssef sellami <youssef-sellami@outlook.com>
2021-11-19 23:25:27 +0100

    added the sending logic for AmazonSES

--- src/Email.NET.AmazonSES/AmazonSESEmailDeliveryProvider.cs
+++ src/Email.NET.AmazonSES/AmazonSESEmailDeliveryProvider.cs
@@ -1,6 +1,9 @@
 ﻿namespace Email.NET.EDP.AmazonSES
 {
+    using Amazon.SimpleEmail;
+    using Amazon.SimpleEmail.Model;
     using System;
+    using System.Linq;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -9,15 +12,43 @@
     public partial class AmazonSESEmailDeliveryProvider : IAmazonSESEmailDeliveryProvider
     {
         /// <inheritdoc/>
-        public EmailSendingResult Send(Message message, params EdpData[] data)
-        {
-            throw new System.NotImplementedException();
-        }
+        public EmailSendingResult Send(NET.Message message, params EdpData[] data)
+            => SendAsync(message, data).ConfigureAwait(false).GetAwaiter().GetResult();
 
         /// <inheritdoc/>
-        public Task<EmailSendingResult> SendAsync(Message message, params EdpData[] data)
+        public async Task<EmailSendingResult> SendAsync(NET.Message message, params EdpData[] data)
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (var client = CreateClient(data))
+                {
+                    var response = await client.SendEmailAsync(CreateMessage(message, data))
+                        .ConfigureAwait(false);
+
+                    if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        return EmailSendingResult.Success(Name)
+                            .AddMetaData("message_id", response.MessageId)
+                            .AddMetaData("request_id", response.ResponseMetadata?.RequestId);
+                    }
+                    
+                    var result = EmailSendingResult.Failure(Name)
+                        .AddMetaData("message_id", response.MessageId)
+                        .AddMetaData("request_id", response.ResponseMetadata?.RequestId);
+
+                    if (!(response.ResponseMetadata is null) && response.ResponseMetadata.Metadata.Any())
+                    {
+                        foreach (var metaData in response.ResponseMetadata.Metadata)
+                            result.AddMetaData(metaData.Key, metaData.Value);
+                    }
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                return EmailSendingResult.Failure(Name).AddError(ex);
+            }
         }
     }
 
@@ -44,6 +75,74 @@
             // validate if the options are valid
             options.Validate();
             _options = options;
+        }
+
+        private AmazonSimpleEmailServiceClient CreateClient(EdpData[] data)
+        {
+            if (!string.IsNullOrEmpty(_options.AwsAccessKeyId)
+                && !string.IsNullOrEmpty(_options.AwsSecretAccessKey)
+                && !string.IsNullOrEmpty(_options.AwsSessionToken))
+            {
+                return new AmazonSimpleEmailServiceClient(_options.AwsAccessKeyId, _options.AwsSecretAccessKey, _options.AwsSessionToken, _options.RegionEndpoint);
+            }
+
+            if (!string.IsNullOrEmpty(_options.AwsAccessKeyId)
+                && !string.IsNullOrEmpty(_options.AwsSecretAccessKey))
+            {
+                return new AmazonSimpleEmailServiceClient(_options.AwsAccessKeyId, _options.AwsSecretAccessKey, _options.RegionEndpoint);
+            }
+
+            return new AmazonSimpleEmailServiceClient(_options.RegionEndpoint);
+        }
+
+        /// <summary>
+        /// create an instance of <see cref="BasicMessage"/> from the given <see cref="Message"/>.
+        /// </summary>
+        /// <param name="message">the message instance</param>
+        /// <param name="data">the edp data instance</param>
+        /// <returns>instance of <see cref="BasicMessage"/></returns>
+        public SendEmailRequest CreateMessage(NET.Message message, EdpData[] data)
+        {
+            var mailMessage = new SendEmailRequest
+            {
+                Message = new Message(),
+                Source = message.From.Address,
+                Destination = new Destination(),
+            };
+
+            if (!(message.Subject is null))
+                mailMessage.Message.Subject = new Content(message.Subject.Content);
+
+            mailMessage.Message.Body = new Body();
+
+            if (!(message.HtmlBody is null))
+                mailMessage.Message.Body.Html = new Content(message.HtmlBody.Content);
+
+            if (!(message.PlainTextBody is null))
+                mailMessage.Message.Body.Text = new Content(message.PlainTextBody.Content);
+
+            if (!(message.ReplyTo is null) && message.ReplyTo.Any())
+            {
+                foreach (var email in message.Bcc)
+                    mailMessage.ReplyToAddresses.Add(email.Address);
+            }
+
+            foreach (var email in message.To)
+                mailMessage.Destination.ToAddresses.Add(email.Address);
+
+            if (!(message.Bcc is null) && message.Bcc.Any())
+            {
+                foreach (var email in message.Bcc)
+                    mailMessage.Destination.BccAddresses.Add(email.Address);
+            }
+
+            if (!(message.Cc is null) && message.Cc.Any())
+            {
+                foreach (var email in message.Cc)
+                    mailMessage.Destination.CcAddresses.Add(email.Address);
+            }
+
+            return mailMessage;
         }
     }
 }
--- src/Email.NET.AmazonSES/Options/AmazonSESEmailDeliveryProviderOptions.cs
+++ src/Email.NET.AmazonSES/Options/AmazonSESEmailDeliveryProviderOptions.cs
@@ -1,10 +1,32 @@
 ﻿namespace Email.NET.EDP.AmazonSES
 {
+    using Amazon;
+
     /// <summary>
     /// the options for configuring the AmazonSES email delivery provider
     /// </summary>
     public class AmazonSESEmailDeliveryProviderOptions
     {
+        /// <summary>
+        /// Get or set the AWS access Key Id.
+        /// </summary>
+        public string AwsAccessKeyId { get; set; }
+        
+        /// <summary>
+        /// Get or set the AWS secret access key.
+        /// </summary>
+        public string AwsSecretAccessKey { get; set; }
+        
+        /// <summary>
+        /// Get or set the AWS session token.
+        /// </summary>
+        public string AwsSessionToken { get; set; }
+
+        /// <summary>
+        /// Get or set the AWS Region you're using for Amazon SES. 
+        /// </summary>
+        public RegionEndpoint RegionEndpoint { get; set; }
+
         /// <summary>
         /// validate if the options are all set correctly
         /// </summary>
