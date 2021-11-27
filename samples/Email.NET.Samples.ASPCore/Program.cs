658a30b20a885bbae2ad6fa477540e43fb698605
Youssef sellami <youssef-sellami@outlook.com>
2021-11-27 19:14:36 +0100

    added ASP core sample code

--- samples/Email.NET.Samples.ASPCore/Program.cs
+++ samples/Email.NET.Samples.ASPCore/Program.cs
@@ -4,17 +4,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 // add Email.Net configuration
-builder.Services.AddEmailNet(op =>
+builder.Services.AddEmailNet(options =>
 {
-    op.PauseSending = false;
-    op.DefaultFrom = new System.Net.Mail.MailAddress("from@email.net");
-    op.DefaultEmailDeliveryProvider = SmtpEmailDeliveryProvider.Name;
+    /* 
+     * if set to true we will not send any email, 
+     * great if we don't want to send emails while testing other functionalities 
+     */
+    options.PauseSending = false;
+
+    /* used to specify the default from to be used when sending the emails */
+    options.DefaultFrom = new MailAddress("from@email.net");
+
+    /* set the default EDP to be used for sending the emails */
+    options.DefaultEmailDeliveryProvider = SmtpEmailDeliveryProvider.Name;
+
+    /* to use a deferent EDP as your default one, just uncomment the one you need */
+    //options.DefaultEmailDeliveryProvider = SocketLabsEmailDeliveryProvider.Name;
+    //options.DefaultEmailDeliveryProvider = AmazonSESEmailDeliveryProvider.Name;
+    //options.DefaultEmailDeliveryProvider = SendgridEmailDeliveryProvider.Name;
+    //options.DefaultEmailDeliveryProvider = MailKitEmailDeliveryProvider.Name;
+    //options.DefaultEmailDeliveryProvider = MailgunEmailDeliveryProvider.Name;
 })
-.UseSmtp(op => op.UseGmailSmtp("", ""))
+.UseSmtp(options => options.UseGmailSmtp("your-email@gmail.com", "password"))
+.UseMailKit(options => options.UseOutlookSmtp("your-email@outlook.com", "password"))
+.UseSocketlabs(apiKey: "", serverId: 0)
 .UseMailgun(apiKey: "", domain: "")
-.UseSendGrid(apiKey: "")
-.UseSocketlabs(apiKey: "", serverId: 1)
-.UseMailKit(op => op.UseGmailSmtp("", ""));
+.UseSendGrid(apiKey: "");
 
 var app = builder.Build();
 
--- samples/Email.NET.Samples.ASPCore/_global.cs
+++ samples/Email.NET.Samples.ASPCore/_global.cs
@@ -1,4 +1,7 @@
-﻿global using Email.NET.EDP.SocketLabs;
+﻿global using Microsoft.AspNetCore.Mvc.RazorPages;
+global using System.Net.Mail;
+
+global using Email.NET.EDP.SocketLabs;
 global using Email.NET.EDP.AmazonSES;
 global using Email.NET.EDP.SendGrid;
 global using Email.NET.EDP.Mailgun;
