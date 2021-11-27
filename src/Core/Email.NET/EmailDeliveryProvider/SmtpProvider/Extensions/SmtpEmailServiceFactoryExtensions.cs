6be6fb2f6a4336b81a55bcc93b8d37a3fc00e75d
Youssef sellami <youssef-sellami@outlook.com>
2021-11-27 18:38:58 +0100

    added Smtp Edp extensions on the EmailServiceFactory

---
+++ src/Core/Email.NET/EmailDeliveryProvider/SmtpProvider/Extensions/SmtpEmailServiceFactoryExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Email.NET
+{
+    using Email.NET.EDP.Smtp;
+    using Email.NET.Factories;
+    using System;
+
+    /// <summary>
+    /// the extensions methods over the <see cref="EmailServiceFactory"/> factory.
+    /// </summary>
+    public static class SmtpEmailServiceFactoryExtensions
+    {
+        /// <summary>
+        /// add the SMTP EDP to be used with your email service.
+        /// </summary>
+        /// <param name="builder">the <see cref="EmailServiceFactory"/> instance.</param>
+        /// <param name="config">the configuration builder instance.</param>
+        /// <returns>instance of <see cref="EmailServiceFactory"/> to enable methods chaining.</returns>
+        public static EmailServiceFactory UseMailKit(this EmailServiceFactory builder, Action<SmtpEmailDeliveryProviderOptions> config)
+        {
+            // load the configuration
+            var configuration = new SmtpEmailDeliveryProviderOptions();
+            config(configuration);
+
+            // validate the configuration
+            configuration.Validate();
+
+            // add the Edp to the emails service factory
+            builder.UseEDP(new SmtpEmailDeliveryProvider(configuration));
+
+            return builder;
+        }
+    }
+}
