9e8de6ba06bc8dfef1e6f4f37dc581597e03a4e7
Youssef sellami <youssef-sellami@outlook.com>
2021-12-17 23:08:50 +0100

    changed Environment.GetEnvironmentVariable to use process instead of machine

--- tests/Email.Net.EDPs.Test/Utilities/EnvVariable.cs
+++ tests/Email.Net.EDPs.Test/Utilities/EnvVariable.cs
@@ -6,7 +6,7 @@
     {
         public static string Load(string name)
         {
-            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Machine)
+            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process)
                 ?? throw new ArgumentNullException(nameof(name), $"failed to load the EnvironmentVariable with name [{name}]");
         }
     }
