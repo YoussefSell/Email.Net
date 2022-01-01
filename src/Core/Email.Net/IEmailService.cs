c11553f9283759c8d724e1366549cb4bdd14622f
Youssef sellami <youssef-sellami@outlook.com>
2022-01-01 16:19:51 +0100

    reorganized methods in IEmailService

--- src/Core/Email.Net/IEmailService.cs
+++ src/Core/Email.Net/IEmailService.cs
@@ -26,6 +26,31 @@
         EmailSendingResult Send(EmailMessage message);
 
         /// <summary>
+        /// Sends the specified email message using the email delivery provider with the given name.
+        /// </summary>
+        /// <param name="message">the email message to be send</param>
+        /// <param name="edp_name">the name of the email delivery provider used for sending the email message.</param>
+        /// <exception cref="ArgumentNullException">the given message instance is null, or the provider name is null</exception>
+        /// <exception cref="EmailDeliveryProviderNotFoundException">couldn't find any EDP with the given name</exception>
+        /// <exception cref="ArgumentException">
+        /// the given message doesn't contain a 'From' (sender) email, 
+        /// and no default 'From' (sender) email is set in the <see cref="EmailServiceOptions.DefaultFrom"/> option supplied to the EmailService instance.
+        /// </exception>
+        EmailSendingResult Send(EmailMessage message, string edp_name);
+
+        /// <summary>
+        /// Sends the specified email message using the given email delivery provider.
+        /// </summary>
+        /// <param name="message">the email message to be send</param>
+        /// <param name="edp">the email delivery provider used for sending the email message.</param>
+        /// <exception cref="ArgumentNullException">the given message instance is null, or the provider is null</exception>
+        /// <exception cref="ArgumentException">
+        /// the given message doesn't contain a 'From' (sender) email, 
+        /// and no default 'From' (sender) email is set in the <see cref="EmailServiceOptions.DefaultFrom"/> option supplied to the EmailService instance.
+        /// </exception>
+        EmailSendingResult Send(EmailMessage message, IEmailDeliveryProvider edp);
+
+        /// <summary>
         /// Sends the specified email message using the default <see cref="IEmailDeliveryProvider"/>.
         /// </summary>
         /// <param name="message">the email message to be send</param>
@@ -51,32 +76,7 @@
         /// the given message doesn't contain a 'From' (sender) email, 
         /// and no default 'From' (sender) email is set in the <see cref="EmailServiceOptions.DefaultFrom"/> option supplied to the EmailService instance.
         /// </exception>
-        EmailSendingResult Send(EmailMessage message, string edp_name);
-
-        /// <summary>
-        /// Sends the specified email message using the email delivery provider with the given name.
-        /// </summary>
-        /// <param name="message">the email message to be send</param>
-        /// <param name="edp_name">the name of the email delivery provider used for sending the email message.</param>
-        /// <exception cref="ArgumentNullException">the given message instance is null, or the provider name is null</exception>
-        /// <exception cref="EmailDeliveryProviderNotFoundException">couldn't find any EDP with the given name</exception>
-        /// <exception cref="ArgumentException">
-        /// the given message doesn't contain a 'From' (sender) email, 
-        /// and no default 'From' (sender) email is set in the <see cref="EmailServiceOptions.DefaultFrom"/> option supplied to the EmailService instance.
-        /// </exception>
         Task<EmailSendingResult> SendAsync(EmailMessage message, string edp_name);
-
-        /// <summary>
-        /// Sends the specified email message using the given email delivery provider.
-        /// </summary>
-        /// <param name="message">the email message to be send</param>
-        /// <param name="edp">the email delivery provider used for sending the email message.</param>
-        /// <exception cref="ArgumentNullException">the given message instance is null, or the provider is null</exception>
-        /// <exception cref="ArgumentException">
-        /// the given message doesn't contain a 'From' (sender) email, 
-        /// and no default 'From' (sender) email is set in the <see cref="EmailServiceOptions.DefaultFrom"/> option supplied to the EmailService instance.
-        /// </exception>
-        EmailSendingResult Send(EmailMessage message, IEmailDeliveryProvider edp);
 
         /// <summary>
         /// Sends the specified email message using the given email delivery provider.
